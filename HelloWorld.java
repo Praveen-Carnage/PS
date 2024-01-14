92b83c10656e7c0bacaf11cbafcfa5e42a46bdae
Praveen-Carnage <r.praveen.snp@gmail.com>
2024-01-14 17:42:56 +0530

    added HelloWorld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld!"); 
-    }
+public class HelloWorld {
+       public static void main(String[] args) {
+		System.out.println("Hello World");
+     }
 }
